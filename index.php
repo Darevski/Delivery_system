f9d2b84c16fd1617be54be961b9c91aee1e10ff5
darevski <mr.darevski@gmail.com>
2016-04-12 00:19:16 +0300

    BackEnd Deleted FPDF library now using TCPDF Using composer

--- index.php
+++ index.php
@@ -10,4 +10,5 @@ session_start();
 ini_set('display_errors', '1');
 error_reporting(E_ALL);
 
-include_once 'Application/BootStrap.php';
+require_once 'Application/vendor/autoload.php';
+require_once 'Application/BootStrap.php';
